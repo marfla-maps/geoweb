712b6d577cce6c6108e1521908cea5cf076e6b9c
marfla-maps <marjanflandia@gmail.com>
2022-01-21 23:08:57 +0100

    3d rutas

---
+++ js/rutas.js
@@ -0,0 +1,33 @@
+function addRutas() {
+
+    var url = 'datos/rutas.geojson';
+    map.addSource('rutas', 
+    { type: 'geojson', 
+    data: url});
+
+         map.addLayer({
+    'id': 'route',
+    'type': 'line',
+    'source': 'rutas',
+    'layout': {
+    'line-join': 'round',
+    'line-cap': 'round'
+    },
+    'paint': {
+    'line-color': '#ff0000',
+    'line-width': 3
+    }
+    });
+
+    function zoomToRutas(valores) {
+
+        var coord = valores.split("/");
+        console.info(coord);
+        map.flyTo({
+                center: [coord[2], coord[1]],
+                zoom: coord[0]
+            });
+    
+        } //fin funcion
+    
+    } //fin funcion
